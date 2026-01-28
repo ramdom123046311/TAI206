93690500e4034a397a7772e779140b8024e327c0
ramdom123046311 <123046311@upq.edu.mx>
2026-01-28 10:16:01 -0600

    Update main.py

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,15 +1,43 @@
 #importaciones
 from fastapi import FastAPI
-
+import asyncio
+from typing import Optional
 
 #Inicializacion del servidor
-app = FastAPI()
+app = FastAPI(
+    title='mi primer API',
+    description='Manuel David Tovar Rodriguez', 
+    version='1.0'
+)
+
+usuarios=[
+    {"id":1,"nombre":"Manuel Tovar","edad":38},
+    {"id":2,"nombre":"Andres Martinez","edad":20},
+    {"id":3,"nombre":"Diego Rubio","edad":21},
+]
 
 #Endpoints
 @app.get("/")
 async def holamundo():
-    return {"mensaje":"Holamundo FastA"}
+    return {"mensaje":"Holamundo FastAPI"}
 
-@app.get("/bienvenidos")
+@app.get("/bienvenidos", tags=['Inicio'])
 async def bienvenido():
     return {"mensaje":"Bienvenidos a tu API REST"}
+
+@app.get("/v1/calificaciones",tags=['Asincronia'])
+async def calificaciones():
+    await asyncio.sleep(6)
+    return {"mensaje":"Tu calificacion en TAI es 10"}
+#opcinal
+@app.get("/v1/usuarios_op/", tags=['Parametro opcionales'])
+async def cunsultaOp(id: Optional[int]=None):
+    await asyncio.sleep(3)
+    if id is not None:
+        return {"Bienvenidos a tu api REST":id }
+
+
+@app.get("/v1/usuarios/{id}", tags=['Parametro obligatorio'])
+async def cunsultaUsuarios(id:int):
+    await asyncio.sleep(3)
+    return {"Bienvenidos a tu api REST":id }
